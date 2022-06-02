fd2f032d4340f0577ceed482df2b41b5976163fd
User <User@DESKTOP-KBKOPOV>
2022-06-02 23:45:45 +0530

    Maven changes commit

---
+++ SeleniumProject/src/pkg1/TC1.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+public class TC1 {
+	
+	@Test
+	public void testcase1()
+	{
+		System.out.println("I am Testcase1");
+	}
+
+}
---
+++ SeleniumProject/src/pkg1/TC2.java
@@ -0,0 +1,12 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+public class TC2 {
+	@Test
+	public void testcase1()
+	{
+		System.out.println("I am Testcase2");
+	}
+
+}
